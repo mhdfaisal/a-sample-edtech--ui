83836c5c841b4c5f55a79b9d0448ff0d0aa58223
Mohd Faisal <mohd@thetrustsearch.com>
2021-02-17 14:52:42 +0530

    add stylings for question card and remove react mdi icons

--- src/Components/shared/QuestionCard/index.js
+++ src/Components/shared/QuestionCard/index.js
@@ -1,46 +1,60 @@
 import React from 'react';
 import Avatar from 'react-avatar';
-import Icon from '@mdi/react';
-import { mdiThumbUpOutline, mdiThumbDownOutline, mdiShareVariantOutline } from '@mdi/js';
 
 import MessageCircleImage from '../../../assets/images/message-circle.png';
 import StarRated from '../../../assets/images/star-rated.png';
 import StarNonRated from '../../../assets/images/non-rated-star.png';
+import ThumbsUp from '../../../assets/images/thumbs-up.png';
+import ThumbsDown from '../../../assets/images/thumbs-down.png';
+import Share from '../../../assets/images/share-2.png';
 
 import './style.css';
 
 const QuestionCard = () => {
 	return (
 		<div className='question__card'>
-			<div className='question__card__avatar'>
-				<Avatar
-					color={Avatar.getRandomColor('sitebase', ['red', 'green', 'blue'])}
-					name='Wim Mostmans'
-					size='56'
-					round
-				/>
-			</div>
-			<div className='question__card__body'>
-				<div>
-					<h3 className='question__card__heading'>¿Cuáles son los múltiplos del 7?</h3>
-					<p className='question__card__description'>
-						La verdad no me queda muy claro cuáles son los múltiplos del 7 porque...
-					</p>
+			<div className='question__card__content'>
+				<div className='question__card__avatar'>
+					<Avatar
+						color={Avatar.getRandomColor('sitebase', ['red', 'green', 'blue'])}
+						name='Wim Mostmans'
+						size='56'
+						round
+					/>
 				</div>
-				<div>
-					<img src={MessageCircleImage} alt='messages' />
-					<span>72</span>
+				<div className='question__card__body'>
+					<div>
+						<h3 className='question__card__heading'>
+							¿Cuáles son los múltiplos del 7?
+						</h3>
+						<p className='question__card__description'>
+							La verdad no me queda muy claro cuáles son los múltiplos del 7 porque...
+						</p>
+					</div>
+				</div>
+				<div className='question__card__comment'>
+					<img
+						src={MessageCircleImage}
+						alt='messages'
+						className='question__card__comment_image'
+					/>
+					<span className='question__card__comment_count'>72</span>
 				</div>
 			</div>
 			<div className='question__card__footer'>
 				<div className='question__card__feedback'>
-					<Icon path={mdiThumbUpOutline} title='Like' size={1} color='#9A9CB5' />
-					<Icon path={mdiThumbDownOutline} title='Dislike' size={1} color='#9A9CB5' />
+					<img src={ThumbsUp} alt='like' className='question__card__like-img' />
+					<img src={ThumbsDown} alt='dislike' className='question__card__dislike-img' />
 				</div>
-				<div>Pregunta juan.c23 en Matemáticas 6º</div>
-				<div>
-					<Icon path={mdiShareVariantOutline} title='share' size={1} color='#9A9CB5' />
-					<img src={StarNonRated} alt='not-rated' />
+				<div className='question__card__posted-by'>
+					<span>
+						Pregunta juan.c23 en{' '}
+						<span className='question__card__subject'>Matemáticas 6º</span>
+					</span>
+				</div>
+				<div className='question__card__social'>
+					<img src={Share} alt='share' className='question__card__share' />
+					<img src={StarRated} alt='favorite' className='question__card__favorite' />
 				</div>
 			</div>
 		</div>
